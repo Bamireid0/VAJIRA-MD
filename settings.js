5a23da35516ba278232dc82421b445d231a9e9e4
Bamireid0 <166797021+Bamireid0@users.noreply.github.com>
2024-07-19 08:14:54 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -11,7 +11,7 @@ const FOOTERNAME = '> @bamireid';
 global.owner = process.env.OWNER_NUMBER  || '2347015983330' ;  // ADD YOUR NUMBER WITHOUT +
     
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || 'youre session id' ,
+SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=WYcwkJ6K#WX5hYdjYcpTLyQF9TZhbKCtY8by4sQRE5p2BcIWnqdY' ,
 travaSend: process.env.TRAVA_SEND || '25' ,   
 MODERATORS : process.env.MODERATORS === undefined ? "2347015983330,2348102925093" : process.env.MODERATORS ,    
 MAX_SIZE: process.env.MAX_SIZE === undefined ? '1536': process.env.MAX_SIZE,/*add this in megabytes*/    
